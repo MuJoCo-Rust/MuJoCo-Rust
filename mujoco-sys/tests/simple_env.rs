1b05ed3c4e4f1fba3f851dc6e46e8c691d1961d3
Ryan Butler <thebutlah@gmail.com>
2020-08-01 06:14:25 -0400

    Added mujoco-sys tests

--- mujoco-sys/tests/simple_env.rs
+++ mujoco-sys/tests/simple_env.rs
@@ -2,7 +2,7 @@ use lazy_static::lazy_static;
 use mujoco_sys::*;
 use std::ffi::{CStr, CString};
 
-const xml: &str = r#"
+const XML: &str = r#"
 <mujoco>
    <worldbody>
       <light diffuse=".5 .5 .5" pos="0 0 3" dir="0 0 -1"/>
@@ -34,9 +34,103 @@ lazy_static! {
             .unwrap()
     )
     .unwrap();
+    static ref XML_NAME: &'static CStr =
+        CStr::from_bytes_with_nul(b"simple.xml\0").unwrap();
+}
+
+fn activate() {
+    assert_eq!(unsafe { mj_activate(MJ_KEY.as_ptr()) }, 1);
+}
+
+fn load_model() -> *mut mjModel {
+    use std::mem::MaybeUninit;
+    let mut vfs: mjVFS = {
+        let mut vfs_uninit: MaybeUninit<mjVFS> = MaybeUninit::uninit();
+        unsafe {
+            mj_defaultVFS(vfs_uninit.as_mut_ptr());
+            vfs_uninit.assume_init()
+        }
+    };
+    {
+        assert_eq!(
+            unsafe {
+                mj_makeEmptyFileVFS(
+                    &mut vfs,
+                    XML_NAME.as_ptr(),
+                    XML.len() as std::os::raw::c_int,
+                )
+            },
+            0
+        );
+        let file_idx = unsafe { mj_findFileVFS(&vfs, XML_NAME.as_ptr()) };
+        assert_ne!(file_idx, -1);
+        let file_buf: *mut std::os::raw::c_void = vfs.filedata[file_idx as usize];
+        let file_buf = file_buf as *mut std::os::raw::c_uchar;
+        unsafe { std::ptr::copy_nonoverlapping(XML.as_ptr(), file_buf, XML.len()) };
+    }
+
+    let model;
+    unsafe {
+        let mut err: Box<[std::os::raw::c_uchar; 1000]> = Box::new([b'\0'; 1000]);
+        model = mj_loadXML(
+            XML_NAME.as_ptr(),
+            &vfs,
+            err.as_mut_ptr() as *mut std::os::raw::c_char,
+            1000,
+        );
+        assert_ne!(
+            model,
+            std::ptr::null_mut(),
+            "Error when loading XML: {}",
+            String::from_utf8(err.to_vec()).unwrap()
+        );
+        mj_deleteVFS(&mut vfs);
+    }
+    model
 }
 
 #[test]
 fn test_activate() {
-    assert_eq!(unsafe { mj_activate(MJ_KEY.as_ptr()) }, 1);
+    activate();
+    // deactivate();
+}
+
+#[test]
+fn test_load_model() {
+    activate();
+    let model = load_model();
+    unsafe { mj_deleteModel(model) };
+    // deactivate();
+}
+
+#[test]
+fn test_make_data() {
+    activate();
+    let model = load_model();
+
+    let data = unsafe { mj_makeData(model) };
+    assert_ne!(data, std::ptr::null_mut());
+    unsafe {
+        mj_deleteData(data);
+        mj_deleteModel(model);
+    }
+    // deactivate();
+}
+
+#[test]
+fn test_simulate() {
+    activate();
+    let model = load_model();
+    let data = unsafe { mj_makeData(model) };
+    assert_ne!(data, std::ptr::null_mut());
+
+    for _ in 0..10 {
+        println!("Doing step");
+        unsafe { mj_step(model, data) };
+    }
+    unsafe {
+        mj_deleteData(data);
+        mj_deleteModel(model);
+    }
+    // deactivate();
 }
